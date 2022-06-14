054a42ae7b959841a1a273c1fb2f1db8e8d71636
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-06-15 02:26:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,14 +5,14 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "TaliaMüzik") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "Efsanestar_bot")
+SESSION_NAME = getenv("BABqhC8y13OWVscqiOHB-Zse6mvEHROKK_rT2PenD7vsHUq_bQOPg78iFFbmQVvckqaAsD2KvX87H_U0O7NdzRHYbg-UDm7YhMXOfEevZrd5qhcjWUKxhfU7_iNXBj1JN9HeuhkHGvWe-9Xmw5YGMXvwXgNkS-Un85CDaBHTSdta5Z8yHr05MPJVr9wq1LlkRDv7CjSK4NsVkB1Bfh6LlORlYNW8_uD7OsVJ_pf2eD8Y8-mzPMz0FEVp3pfk9P5PYjvlmTaVRST14Q6YUCzSGtLqYfzmSBQLPyjMn8qLRaEMt4knncGiKCoFkIDsJlsvjNwqXyB525Bg1wI53lMIYjWqAAAAAFsLb9EA", "session")
+BOT_TOKEN = getenv("5211551773:AAEGPgnO6C-1GDiwn-R6QY4gl6JW_OOaROE")
+BOT_NAME = getenv("MytMusic", "MytMüzik") 
+API_ID = int(getenv("3939406"))
+API_HASH = getenv("e11d0eaec136b1047974ab098041e9f2")
+BOT_USERNAME = getenv("BOT_USERNAME", "Myt_Music_Bot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "59"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
